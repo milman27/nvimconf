6df73fad36d6dd24447ac65d75055e632179eefd
Max <win27mwb@gmail.com>
2025-06-30 17:40:33 +0200

    updaet

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ then
     echo 'Please run as root'
     exit
 fi    
-
-pacman -Syy nvim ccls 
+pacman -Syy nvim ccls git 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 curl -sL install-node.vercel.app/lts | bash
 mv -rf ./nvim/ /home/$SUDO_USER/.config/
