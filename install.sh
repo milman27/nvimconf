a68305b89f6147744218da7a504f049c9116eff2
Max <win27mwb@gmail.com>
2025-06-30 18:21:27 +0200

    updaet

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-if [ $(whoami) != 'root' ] 
-then
-    echo 'Please run as root'
-    exit
-fi    
-pacman -Syy n--needed git base-devel vim ccls 
+sudo pacman -Syy n--needed git base-devel vim ccls 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay -Sy nvim-packer-git
 curl -sL install-node.vercel.app/lts | bash
-mv -f ./nvim/ /home/$SUDO_USER/.config/
+mv -f ./nvim/ ~/.config/
