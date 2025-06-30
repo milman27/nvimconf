7e2ac856036992870ab0060fb7c28f9763166613
Max <win27mwb@gmail.com>
2025-06-30 18:56:40 +0200

    typos

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo pacman -Syy --needed git base-devel nvim ccls 
+StartDir = $(pwd)
+sudo pacman -Syy --needed git base-devel nvim ccls man 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay -Sy nvim-packer-git
 curl -sL install-node.vercel.app/lts | sudo bash
+cd StartDir 
+sudo mandb
 mv -f ./nvim/ ~/.config/
